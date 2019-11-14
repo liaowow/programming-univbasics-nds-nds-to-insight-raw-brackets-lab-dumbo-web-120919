6ef103520c3b6eba9b9542d3bccf0b223a9357fa
Annie Liao <liaowow@yahoo.com>
2019-11-14 23:41:05 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -12,19 +12,21 @@ def directors_totals(nds)
   # Use loops, variables and the accessing method, [], to loop through the NDS
   # and total up all the
   result = {}
+  total = 0
   directors_index = 0
   while directors_index < nds.count do
     current_director = nds[directors_index][:name]
     
     movies_index = 0
-    while movies_index <  
-    result[current_director] = nds[directors_index][:movies][:]
-    
-    
+    while movies_index <  nds[directors_index][:movies].count do
+      result[current_director] = nds[directors_index][:movies][:worldwide_gross]
+      movies_index += 1
+    end
     directors_index += 1
   end
   #
   #
   # Be sure to return the result at the end!
-  nil
+  result
+  total
 end
